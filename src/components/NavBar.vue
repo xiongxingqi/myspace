ecdcccde4a79f0a74aa83a11d40c271e42509cec
xxq <1269504317@qq.com>
2024-08-12 11:00:57 +0800

    配置路由并解决Feature flag __VUE_PROD_HYDRATION_MISMATCH_DETAILS__ is not explicitly defined. Where do I define it?警告

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,28 +2,28 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">myspace</a>
+                <a class="navbar-brand" href="/">myspace</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">首页</a>
+                    <a class="nav-link active" aria-current="page" href="/">首页</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#">好友列表</a>
+                    <a class="nav-link" href="/friendsList">好友列表</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#">用户动态</a>
+                    <a class="nav-link" href="/userDynamics">用户动态</a>
                     </li>
                 </ul>
                 <ul class="navbar-nav mb-2 mb-lg-0">
                     <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">登录</a>
+                    <a class="nav-link active" aria-current="page" href="/login">登录</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#">注册</a>
+                    <a class="nav-link" href="/register">注册</a>
                     </li>
                 </ul>
                 </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import FriendsListView from '../views/FriendsListView.vue';
+import LoginView from '../views/LoginView.vue';
+import NotFoundView from '../views/NotFoundView.vue';
+import RegisterView from '../views/RegisterView.vue';
+import UserDynamicsView from '../views/UserDynamicsView.vue';
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
   },
+  {
+    path: '/friendsList',
+    name: 'friendsList',
+    component: FriendsListView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterView
+  },
+  {
+    path: '/userDynamics',
+    name: 'userDynamics',
+    component: UserDynamicsView
+  },
+  {
+    path: '/notFound',
+    name: 'notFound',
+    component: NotFoundView
+  }
 ]
 
 const router = createRouter({
