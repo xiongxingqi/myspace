b28d8b22b052cfc5d72c255b8d0626bb10fa57d4
xxq <1269504317@qq.com>
2024-08-12 20:27:33 +0800

    使用store维护全局属性,并模块化user信息

---
+++ src/store/ModuleUser.js
@@ -0,0 +1,18 @@
+const ModuleUser = {
+    state: {
+        username: "",
+        id: "",
+        firstName: "",
+        lastName: "",
+    },
+    getters: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    modules: {
+    }
+};
+//模块化维护全局状态
+export default ModuleUser;
